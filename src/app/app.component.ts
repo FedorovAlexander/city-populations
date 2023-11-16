c645de3e7da5bdbeea119ba857f7c05df395b145
Alexander Fedorov <hamster2240@gmail.com>
2023-11-16 10:33:35 +0200

    Create a map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import mapboxgl from 'mapbox-gl';
+import config from '../assets/config';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cities';
+  map!: mapboxgl.Map;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.map = new mapboxgl.Map({
+      accessToken: config.MAPBOX_ACCESS_TOKEN,
+      container: 'map',
+      style: 'mapbox://styles/mapbox/dark-v10',
+      zoom: 12,
+      center: [-77.042754, -12.046373],
+    });
+  }
 }
